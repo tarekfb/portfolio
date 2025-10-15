112d5110c5aeca0e1d465345bf081a65e51e0033
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-10-15 14:51:45 +0800

    borders

--- components/TechStackIcon.tsx
+++ components/TechStackIcon.tsx
@@ -4,7 +4,7 @@ import { Tech } from "./Projects";
  
 export default function TechStackIcon({ tech }: { tech: Tech }) {
   return (
-    <div className="size-9 -mr-1.5 rounded-full overflow-hidden flex items-center justify-center p-0.5 bg-accent">
+    <div className="size-9 -mr-1.5 rounded-full overflow-hidden flex items-center justify-center p-0.5 bg-background border border-solid border-accent">
       <div className="rounded-full p-1.5 flex items-center justify-center bg-background">
       <StackIcon name={tech} />
       </div>
