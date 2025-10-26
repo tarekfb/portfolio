48a12b4d52574bb9278abb58a7f12e1d71e54ee5
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-10-26 18:55:36 +0800

    improved usage and code of wavy dividers

--- components/RandomizedDivider.tsx
+++ components/RandomizedDivider.tsx
@@ -1,39 +1,43 @@
 "use client";
-
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { DividerContext } from "./context/DividerProvider";
 import { motion } from "framer-motion";
 
 export default function RandomizedDivider() {
-    const { dividerPathData, dividerPathData2, dividerPathData3 } = useContext(DividerContext);
+    const { firstPath, secondPath, thirdPath } = useContext(DividerContext);
+
     return (
         <>
-            <div className="-mb-32 md:-mb-72">
-                <motion.svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
-                    <motion.path initial={false} className="fill-accent-light"
-                        animate={{ d: dividerPathData3 }}
+            <div className="-mb-22 md:-mb-72">
+                <svg viewBox="0 0 1440 320" className="w-full">
+                    <motion.path
+                        className="fill-accent-light stroke-accent-light stroke-2"
+                        initial={false}
+                        animate={{ d: thirdPath }}
                         transition={{ duration: 1, ease: "easeInOut" }}
                     />
-                </motion.svg>
-                <div className="h-24 md:h-34 bg-accent-light"></div>
-            </div>
+                </svg>
+                <div className="h-12 md:h-34 bg-accent-light"></div>
+             </div>
             <div className="-mb-32 md:-mb-66">
-                <motion.svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
-                    <motion.path initial={false} className="fill-accent-lighter"
-                        animate={{ d: dividerPathData2 }}
+                <svg viewBox="0 0 1440 320" className="w-full">
+                    <motion.path
+                        className="fill-accent-lighter stroke-accent-lighter stroke-2 "
+                        initial={false}
+                        animate={{ d: secondPath }}
                         transition={{ duration: 1, ease: "easeInOut" }}
                     />
-                </motion.svg>
-                <div className="h-24 md:h-34 bg-accent-lighter"></div>
+                </svg>
+                <div className="h-24 bg-accent-lighter"></div>
             </div>
-
-            <motion.svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
-                <motion.path initial={false} className="fill-accent-lightest"
-                    animate={{ d: dividerPathData }}
+            <svg viewBox="0 0 1440 320" className="w-full">
+                <motion.path
+                    className="fill-accent-lightest stroke-accent-lightest stroke-2"
+                    initial={false}
+                    animate={{ d: firstPath }}
                     transition={{ duration: 1, ease: "easeInOut" }}
                 />
-            </motion.svg>
-            {/* <div className="h-24 bg-accent/25"></div> */}
+            </svg>
         </>
     )
 }
