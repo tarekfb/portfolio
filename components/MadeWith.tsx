cff49900dd334896dc9476ad49eeb6253ada0060
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-10-27 12:38:14 +0800

    add popoer for mobile tech stacks

--- components/MadeWith.tsx
+++ components/MadeWith.tsx
@@ -7,7 +7,7 @@ const techs: Tech[] = ["react", "nextjs", "typescript", "tailwindcss"]
 export default function MadeWith() {
     const { theme } = useTheme()
 
-    return (<div className="flex items-center justify-center gap-x-2">Made with
+    return (<div className="flex items-center justify-center gap-x-2 text-muted dark:text-foreground/75">Made with
         <div className="space-x-1.5">
             {techs.map(
                 (tech, i) => (<TechStackIcon tech={tech} variant={tech === "nextjs" && theme === 'light' ? "light" : "dark"} key={i} size="md" />)
--- components/TechStackIcon.tsx
+++ components/TechStackIcon.tsx
@@ -5,27 +5,51 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 interface TechStackIconProps {
   tech: Tech;
   size?: "md" | "lg";
-  variant?: "light" | "dark" | "grayscale" ; 
+  variant?: "light" | "dark" | "grayscale";
 }
 
-export default function TechStackIcon({ tech, size = "lg", variant = "dark"}: TechStackIconProps) {
+function Icon({ tech, size = "lg", variant = "dark" }: TechStackIconProps) {
   return (
-    <Tooltip>
-      <TooltipTrigger>
-        <div className={`size-${size === "md" ? "8" : "9"} -mr-1.5 rounded-full overflow-hidden flex items-center justify-center p-0.5 bg-background border border-solid border-accent`}>
-          <div className="rounded-full p-1.5 flex items-center justify-center bg-background">
-            <StackIcon name={tech} variant={variant}/>
-          </div>
-        </div>
-      </TooltipTrigger>
-      <TooltipContent>
-        <p>{tech}</p>
-      </TooltipContent>
-    </Tooltip>
-    
+    <div className={`size-${size === "md" ? "8" : "9"} -mr-1.5 rounded-full overflow-hidden flex items-center justify-center p-0.5 bg-background border border-solid border-accent`}>
+      <div className="rounded-full p-1.5 flex items-center justify-center bg-background">
+        <StackIcon name={tech} variant={variant} />
+      </div>
+    </div>
+  )
+}
+
+export default function TechStackIcon({ tech, size = "lg", variant = "dark" }: TechStackIconProps) {
+  return (
+    <>
+      <div className="inline md:hidden">
+        <Popover>
+          <PopoverTrigger>
+            <Icon tech={tech} size={size} variant={variant} />
+          </PopoverTrigger>
+          <PopoverContent>
+            <p>{tech}</p>
+          </PopoverContent>
+        </Popover>
+      </div>
+      <div className="hidden md:inline">
+        <Tooltip>
+          <TooltipTrigger>
+            <Icon tech={tech} size={size} variant={variant} />
+          </TooltipTrigger>
+          <TooltipContent>
+           { <p>{tech}</p>}
+          </TooltipContent>
+        </Tooltip>
+      </div>
+    </>
   );
 }
