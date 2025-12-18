6d49ef302e7de5766fd7c94e023035e6c0fce3b7
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-12-18 21:27:09 +0800

    inverted divider

--- components/RandomizedBlob.tsx
+++ components/RandomizedBlob.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 export default function RandomizedBlob() {
     const { blobPathData } = useContext(BlobContext);
     return (
-        <motion.svg viewBox="0 0 250 250" className="w-72 h-72 md:w-96 md:h-96 absolute bottom-20 md:bottom-30 right-5 md:right-15" xmlns="http://www.w3.org/2000/svg">
+        <motion.svg viewBox="0 0 250 250" className="w-72 h-72 md:w-96 md:h-96 absolute bottom-20 md:bottom-30 right-5 md:right-15z" xmlns="http://www.w3.org/2000/svg">
             <motion.path className="fill-accent/50" 
             animate={{d: blobPathData}}
             transition={{duration: 1}}
